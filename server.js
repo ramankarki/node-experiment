7a40987fa075d83dbc9d93cea0e6ceb0d8a2541e
ramankarki <ramankarki40@gmail.com>
2022-03-04 18:54:03 +0545

    using pm2

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const cluster = require('cluster');
-const os = require('os');
 const app = require('./app');
 
 const PORT = process.env.NODE_ENV || 5000;
-const LOGICAL_CPUS = os.cpus().length;
 
-if (cluster.isMaster) {
-  console.log('Master thread is running...');
-  for (let i = 0; i < LOGICAL_CPUS; i++) {
-    cluster.fork();
-    console.log(`${i + 1} Worker Thread running..`);
-  }
-} else {
-  app.listen(PORT);
-}
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+
+// pm2 start server.js -l logs.txt -i max
+// pm2 reload server
