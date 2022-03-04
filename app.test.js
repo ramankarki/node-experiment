8ceae9941f2aa30c34e8f816f47ca9ceeee4b9c7
ramankarki <ramankarki40@gmail.com>
2022-03-05 00:32:16 +0545

    some changes for testing api with mongo

--- app.test.js
+++ app.test.js
@@ -1,10 +1,20 @@
 const request = require('supertest');
 const app = require('./app');
 
-describe('Test GET /', () => {
-  test('It should respond with 200 success', async () => {
-    const response = await request(app).get('/').expect(200);
+describe('Testing API', () => {
+  beforeAll(async () => {
+    // declare this function in separate file to be used in server and here.
+    await mongoConnect();
+  });
 
-    expect(response.body.status).toEqual('success');
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  describe('Test GET /', () => {
+    test('It should respond with 200 success', async () => {
+      const response = await request(app).get('/').expect(200);
+      expect(response.body.status).toEqual('success');
+    });
   });
 });
