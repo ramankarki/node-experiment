a553041d782acf8909fdfb68ba3581cc84ba42a2
ramankarki <ramankarki40@gmail.com>
2022-03-05 22:05:08 +0545

    using socket.io

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const app = require('./app');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  },
+});
 
 const PORT = process.env.NODE_ENV || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
 
 // pm2 start server.js -l logs.txt -i max
 // pm2 reload server
+
+io.on('connection', (socket) => {
+  // console.log('User connected -', socket.id);
+
+  socket.on('ready', (data) => console.log(data, '-', socket.id));
+
+  socket.emit('server ready', 'server le emit gareko data');
+});
