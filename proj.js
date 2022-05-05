acb603faaa9b7217652a5b875c01ed66b167aaa0
Raman Karki <ramankarki40@gmail.com>
2022-05-05 16:44:16 +0545

    some more testing

--- proj.js
+++ proj.js
@@ -12,5 +12,21 @@ if (process.env.NODE_ENV !== 'production') {
 }
 let key = process.env.key;
 
+app.get('/:sectionName', async (req, res) => {
+  try {
+    const data = await axios.get(
+      `https://content.guardianapis.com/search?api-key=${key}&q=${
+        req.params.sectionName
+      }&page=${
+        req.query.page || 1
+      }&show-fields=headline,standfirst,lang,publication,lastModified`
+    );
+
+    res.json(data.data.response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 const port = process.env.port || 8000;
 app.listen(port, () => console.log('Server running successfully'));
